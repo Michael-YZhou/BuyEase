c70d638a77d8c29f2a29d07021432c26d2b2cef7
Yang Zhou <yangzhouinter@gmail.com>
2024-10-10 20:24:31 +1100

    use LinkContainer to wrap Navbar.Brand and Nav.Link in Header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../../assets/logo.png";
 import "./Header.css";
 
@@ -10,19 +11,30 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="ShopEase" />
-            BuyEase
-          </Navbar.Brand>
+          {/* we are using LinkContainer to wrap the Navbar.Brand and Nav.Link components, 
+              because we want to use the react-router-bootstrap package to navigate to the cart 
+              and login pages and we can't use the Link component from react-router-dom directly with 
+              the Nav.Link component from react-bootstrap */}
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="ShopEase" />
+              BuyEase
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser /> Sign In
-              </Nav.Link>
+              {/* use LinkContainer to wrap the Nav.Link components */}
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
