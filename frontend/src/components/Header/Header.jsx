16bb9536631b346596981cb2bdfabc34242501d5
Yang Zhou <yangzhouinter@gmail.com>
2024-10-10 12:33:37 +1100

    add logo and css to the Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
-import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../../assets/images/logo.png";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ShopEase</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="ShopEase" />
+            BuyEase
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
