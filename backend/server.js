3d0d2d607a5a436f5fbdd94d4259973bbbf081ce
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-03-30 18:23:21 +1100

    create users routes at the backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config(); // Load environment variables from a .env file into process.env
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 5000; // Default port is 5000
 
 connectDB(); // Connect to the MongoDB database
@@ -17,7 +18,8 @@ app.get("/", (req, res) => {
 });
 
 // the /api/products route is handled by the productRoutes router
-app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes); // Use product routes
+app.use("/api/users", userRoutes); // Use user routes
 
 // Middleware to handle errors for routes that are not found
 app.use(notFound);
