6642c413f2d73d0ede7038941a47486c1f35588f
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-04-02 20:06:05 +1100

    add the protect and admin middleware to the user routes which require authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,23 +10,26 @@ import {
   deleteUser,
   updateUser,
 } from "../controllers/userController.js"; // Import user controller functions
+import { protect, admin } from "../middleware/authMiddleware.js"; // Import middleware for authentication and authorization
 
 // router is an instance of express.Router that we use to define our routes
 const router = express.Router();
 
 // import route handlers from the controller
 // router.route() can define multiple actions on a single route
-router.route("/").post(registerUser).get(getUsers); // Fetch all users
+// add the protect middleware to the routes that require authentication
+// and the admin middleware to the routes that require admin privileges
+router.route("/").post(registerUser).get(protect, admin, getUsers); // Fetch all users
 router.post("/logout", logoutUser); // Logout user
 router.post("/login", authUser); // Login user
 router
   .route("/profile")
-  .get(getUserProfile) // Get user profile (user info is in the token, no need to pass it in the request)
-  .put(updateUserProfile); // Update user profile
+  .get(protect, getUserProfile) // Get user profile (user info is in the token, no need to pass it in the request)
+  .put(protect, updateUserProfile); // Update user profile
 router
   .route("/:id")
-  .get(getUserById) // Get user by ID
-  .delete(deleteUser) // Delete user
-  .put(updateUser); // Update user
+  .get(protect, admin, getUserById) // Get user by ID
+  .delete(protect, admin, deleteUser) // Delete user
+  .put(protect, admin, updateUser); // Update user
 
 export default router; // Export the router
