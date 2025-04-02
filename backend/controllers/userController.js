9ee4d3d05b2a8b05ea08379ff0dcec81a34a2173
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-04-02 16:26:17 +1100

    create a JWT at backend, set it in an http only cookie and send back with the response

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js"; // Import the Product model
+import jwt from "jsonwebtoken"; // Import the jsonwebtoken library
 
 /**
  * @desc Auth uer & get token
@@ -14,12 +15,22 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     // If user exists and password matches, send user data and token
     // The getSignedJwtToken method is a custom method defined in the User model that generates a JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    }); // Generate JWT token
+    // Set the JWT in a cookie with options
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Set to true if in production to use secure cookies with HTTPS
+      sameSite: "Strict", // Set SameSite attribute to Strict to prevent CSRF attacks
+      maxAge: 30 * 24 * 60 * 60 * 1000, // Set cookie expiration to 30 days in milliseconds
+    });
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      // token: user.getSignedJwtToken(), // Generate JWT token
+      // token: user.getSignedJwtToken(), // Don't send the token in the response, instead send it in the cookie
     });
   } else {
     // If user doesn't exist or password doesn't match, send an error response
