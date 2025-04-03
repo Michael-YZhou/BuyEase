317b73bbd0d3d15bc31e1b21f3ec5166715ce49a
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-04-03 18:19:55 +1100

    create a Form Container to wrap the Login and Register form and center them

---
+++ frontend/src/components/FormContainer/FormContainer.jsx
@@ -0,0 +1,18 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+// This component is used to wrap the form elements in a container
+// and center them on the screen. It is used in the Login and Register pages.
+function FormContainer({ children }) {
+  return (
+    <Container>
+      {/* on medium screen, justify the content to the center */}
+      <Row className="justify-container-md-center">
+        <Col xs={12} md={6}>
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default FormContainer;
