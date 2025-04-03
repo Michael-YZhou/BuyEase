ebc34c2547eb8a5609bedc9ab215d81a90dfb969
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-04-03 18:10:21 +1100

    create the usersApiSlice which contains the login endpoint, and inject it in to the apiSlice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,17 @@
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+// Create a new slice for the product API and inject the endpoints to the parent slice
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: USERS_URL + "/auth",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
